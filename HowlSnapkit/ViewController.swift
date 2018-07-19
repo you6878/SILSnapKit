5429e7f1e5cd8d442de13b39cf380da020b8f4a3
Myeongsic <you6878@icloud.com>
2018-07-19 21:40:28 +0900

    Initial Commit

---
+++ HowlSnapkit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HowlSnapkit
+//
+//  Created by 유명식 on 2018. 7. 19..
+//  Copyright © 2018년 유명식. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HowlSnapkitTests/HowlSnapkitTests.swift
@@ -0,0 +1,36 @@
+//
+//  HowlSnapkitTests.swift
+//  HowlSnapkitTests
+//
+//  Created by 유명식 on 2018. 7. 19..
+//  Copyright © 2018년 유명식. All rights reserved.
+//
+
+import XCTest
+@testable import HowlSnapkit
+
+class HowlSnapkitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
